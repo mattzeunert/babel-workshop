8a1c40c4cb66b7713a78b3f4288c804e99f79d6b
Matt Zeunert <matt@mostlystatic.com>
2017-07-03 21:54:14 +0800

    Babel plugin exercises

--- exercises/10_plugins.js
+++ exercises/10_plugins.js
@@ -1,3 +1,101 @@
+// This is a Babel plugin
+// In the node visitors you can update or replace the node
+function changeAllStringsToHiPlugin(babel){
+    // Turns `"a" + "b"` into `"Hi" + "Hi"`
+    return {
+        visitor: {
+            StringLiteral: function(path){
+                path.node.value = "Hi"
+            }
+        }
+    }
+}
+
+// Should turn `1 + 2` into `Math.random() + Math.random()`
+function turnNumbersIntoMathRandomCalls(babel) {
+    const t = babel.types
+    return {
+        visitor: {
+            // You can path.replaceWith(newNode) to replace the current AST node
+            // START_SOLUTION
+            NumericLiteral: function(path) {
+                const call = t.callExpression(
+                    t.memberExpression(t.identifier("Math"), t.identifier("random")),
+                    []
+                )
+                path.replaceWith(call)
+            }
+            // END_SOLUTION
+        }
+    }
+}
+
+
+// Turns ES2016 `a ** b` into ES5 `Math.pow(a, b)`
+function turnExponentiationOperatorToMathPowCallPlugin(babel) {
+    const t = babel.types
+    return {
+        visitor: {
+            // START_SOLUTION   
+            BinaryExpression: function (path) {
+                if (path.node.operator !== "**") {
+                    return
+                }
+                let addCall = t.callExpression(
+                    t.memberExpression(t.identifier("Math"), t.identifier("pow")),
+                    [path.node.left, path.node.right]
+                )
+                path.replaceWith(addCall)
+            }
+            // END_SOLUTION
+        }
+    }
+}
+
+// Renames the variable `sth` to `something` in the function called `doStuff`
+// Example: `function doStuff() {var sth = 5; sth++}` => `function doStuff(var something = 5; something++)`
+function turnSthToSomethingPlugin(babel) {
+    return {
+        visitor: {
+            // You can use `path.scope.rename(oldName, newName)` to rename a variable in the current scope
+            // START_SOLUTION
+            FunctionDeclaration: function(path) {
+                if (path.node.id.name === "doStuff") {
+                    path.scope.rename("sth", "something");
+                }
+            }
+            // END_SOLUTION
+        }
+    }
+}
+
+
+import * as babel from "babel-core"
+function applyPlugin(code, plugin) {
+    // Babel `transform` combines these three steps:
+    // 1) Parsing
+    // 2) Traversing the AST and changing it
+    // 3) Generating code from the updated AST
+    let transformationResult = babel.transform(code, {
+        plugins: [plugin]
+    })
+    return transformationResult.code
+}
+export function changeAllStringsToHi(code) {
+    return applyPlugin(code, changeAllStringsToHiPlugin)
+}
+export function turnNumbersIntoMathRandom(code) {
+    return applyPlugin(code, turnNumbersIntoMathRandomCalls)
+}
+export function turnExponentiationOperatorToMathPowCall(code) {
+    return applyPlugin(code, turnExponentiationOperatorToMathPowCallPlugin)
+}
+export function turnSthToSomething(code) {
+    return applyPlugin(code, turnSthToSomethingPlugin)
+}
+
+
+
 // babel plugin scleanup task: transform cake["hi"]  to cake.hi
 
 // log every assignment that is made
@@ -10,7 +108,7 @@ FunctionDeclaration(path) {
 -   return n * n;
 + function square(x) {
 +   return x * x;
-  }
+  }N
 
   */
 
---
+++ exercises/10_plugins.test.js
@@ -0,0 +1,43 @@
+import * as plugins from "./10_plugins"
+
+describe("Babel Plugins", () => {
+    function runTests(tests, fn) {
+        Object.keys(tests).forEach((beforeCode) => {
+            const afterCode = tests[beforeCode]
+            test("Turns `" + beforeCode + "` into `" + afterCode + "`", () => {
+                expect(fn(beforeCode)).toBe(afterCode)
+            })
+        })
+    }
+
+    test("stuff", () => {
+        var newCode = plugins.changeAllStringsToHi(`var a = "a";a += "b"`)
+        expect(newCode).toBe(`var a = "Hi";a += "Hi";`)
+    })
+
+    describe("turnNumbersIntoMathRandomPlugin", () => {
+        runTests({
+            "5 + 1": `Math.random() + Math.random();`,
+            "a + 1": `a + Math.random();`,
+            "function inc(a){ return a + 1 }": `function inc(a) {\n  return a + Math.random();\n}`
+        }, plugins.turnNumbersIntoMathRandom)
+    })
+
+    describe("turnExponentiationOperatorToMathPowCall", () => {
+        runTests({
+            "5 ** 2": `Math.pow(5, 2);`,
+            "a ** 2": `Math.pow(a, 2);`,
+            "a - 1": `a - 1;`,
+            "1 ** 2 ** 3": `Math.pow(1, Math.pow(2, 3));`
+        }, plugins.turnExponentiationOperatorToMathPowCall)
+    })
+
+    describe("turnSthToSomething", () => {
+        runTests({
+            "a + b": "a + b;",
+            "function doStuff() { var sth = 1; sth++; return sth }": "function doStuff() {\n  var something = 1;something++;return something;\n}",
+            "function doThings() { var sth = 5}": "function doThings() {\n  var sth = 5;\n}"
+        }, plugins.turnSthToSomething)
+    })
+    
+})
