50f33220dfd4bcee7116d503593f617ff2e21967
gitztech <gjpyzer+github@gmail.com>
2017-07-15 12:06:31 +0100

    Add my solutions and notes on things that threw me briefly

--- exercises/02_construction.js
+++ exercises/02_construction.js
@@ -3,18 +3,22 @@ import * as t from "babel-types"
 // Should return a string literal AST node with the value "Hello"
 export function getHelloStringLiteral(){
     // See the babel-types documentation here: https://babeljs.io/docs/core-packages/babel-types/#apistring-literal
+	return t.stringLiteral('Hello');
     
 }
 
 // Should return AST node equivalent to this code: `alert("Hello")`
 export function getAlertCall(){
     // For help, try inspecint `alert("Hello")` in AST Explorer
+	return t.callExpression(t.identifier('alert'), [t.stringLiteral('Hello')]);
     
 }
 
 // Should return AST node equivalent to this code: `Math.pow({{base}}, {{exponent}})`
 export function getMathPowCall(base, exponent){
-    
+    return t.callExpression(
+    	t.memberExpression(t.identifier('Math'), t.identifier('pow')), [base, exponent]
+	);
 }
 
 // Manually constructing the AST is tedious, so you can use Babel template instead
@@ -29,6 +33,12 @@ import template from "babel-template";
 
 // Should return AST node equivalent to this code: `Math.pow({{base}}, {{exponent}})`
 export function getMathPowCallUsingBabelTemplate(base, exponent){
+	var buildMathPowCall = template(`Math.pow(BASE, EXPONENT)`);
+
+	return buildMathPowCall({
+		BASE: base,
+		EXPONENT: exponent
+	});
     
 }
 
@@ -39,5 +49,6 @@ import generate from "babel-generator";
 
 // Should return a string like this: `Math.pow({{base}}, {{exponent}})`
 export function getMathPowCallAsString(base, exponent) {
-    
+	let ast = getMathPowCallUsingBabelTemplate(base, exponent);
+	return generate(ast).code;
 }
